0958002afb8cafceb3192cc77a621c0cfd472681
Ivan Neradovic <i.neradovic@levi9.com>
2017-02-21 11:52:08 +0100

    making all the requested changes to complete this project

--- src/assets/main.js
+++ src/assets/main.js
@@ -2,4 +2,36 @@ $(function() {
 
   // your code will go here
 
+  	$.ajax({
+  		url: "https://www.codeschool.com/users/ivan82cs.json", 
+  		dataType: 'jsonp',
+  		success: function(response){
+
+    		var completed = response.courses.completed;
+    		$.each(completed, function(index, item) {
+    			console.log(item);
+    			$("#badges ").append(
+    				"<div class='course'>" + 
+    					"<h3>" + item.title + "</h3>" +
+    					"<img src='" + item.badge + "' alt='Codeschool " + item.title + " Badge'>" + 
+    					"<a href='" + item.url + "' target='_blank' class='btn btn-primary'>See Course</a>" + 
+    				"</div>"
+    			);
+
+			});
+
+    		//console.log(response.courses.completed);
+  		}
+	});
+
+
+
+
+
+
+
+
+
+
+
 });
